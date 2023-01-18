c066500dc579e8a23e3790bd5fb4eea67c82cd0f
t213005 <121270907+t213005@users.noreply.github.com>
2023-01-18 01:33:48 +0000

    1st

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from scipy.optimize import minimize
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
     st.title("Function Optimizer")
@@ -15,12 +17,23 @@ def main():
     # Get initial guess from user
     x0 = st.number_input("Enter an initial guess for x:")
 
+    # Get optimization method
+    method = st.selectbox("Select an optimization method:", ["BFGS", "CG", "L-BFGS-B", "SLSQP"])
+
     # Find the minimum value
-    res = minimize(func, x0)
+    res = minimize(func, x0, method=method)
 
     # Show results
     st.write(f"Minimum value: {res.fun}")
     st.write(f"Minimum occurs at x = {res.x}")
+
+    # Plot the function and optimization result
+    x = np.linspace(-10, 10, 100)
+    y = func(x)
+    plt.plot(x, y, label="Function")
+    plt.scatter(res.x, res.fun, c='r', label="Minimum")
+    plt.legend()
+    st.pyplot()
 
 if __name__ == "__main__":
     main()
