7e9f7e036c919f1fe16f9144cb5f0f7da830d0ff
t213005 <121270907+t213005@users.noreply.github.com>
2023-01-18 01:09:55 +0000

    1st

--- app.py
+++ app.py
@@ -1,50 +1,26 @@
 import streamlit as st
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
-from keras.utils import to_categorical
+from scipy.optimize import minimize
 
-# MNISTデータの読み込み
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+def main():
+    st.title("Function Optimizer")
+    st.write("This app helps you find the minimum value of a function.")
 
-# 入力データの整形
-X_train = X_train.reshape(60000, 784)
-X_test = X_test.reshape(10000, 784)
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
+    # Get function from user
+    func_str = st.text_input("Enter a function in terms of x:")
 
-# ターゲットデータの整形
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
+    # Define the optimization function
+    def func(x):
+        return eval(func_str)
 
-# アルゴリズムの選択
-st.sidebar.title("Optimization Algorithm")
-algorithm = st.sidebar.selectbox("Select optimization algorithm", ["Adam", "SGD", "Adagrad"])
+    # Get initial guess from user
+    x0 = st.number_input("Enter an initial guess for x:")
 
-# ニューラルネットワークの構築
-model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dense(10, activation='softmax'))
+    # Find the minimum value
+    res = minimize(func, x0)
 
-# コンパイル
-if algorithm == "Adam":
-    optimizer = Adam()
-elif algorithm == "SGD":
-    optimizer = SGD()
-else:
-    optimizer = Adagrad()
+    # Show results
+    st.write(f"Minimum value: {res.fun}")
+    st.write(f"Minimum occurs at x = {res.x}")
 
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-
-# 学習
-epochs = st.sidebar.slider("Select number of epochs", 1, 30)
-model.fit(X_train, y_train, epochs=epochs, verbose=1)
-
-# 評価
-score = model.evaluate(X_test, y_test, verbose=0)
-st.write("Test loss:", score[0])
-st.write("Test accuracy:", score[1])
-
+if __name__ == "__main__":
+    main()
