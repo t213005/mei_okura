e3567bc2633a23f2416ea9a3ea10972c1f2391c8
t213005 <121270907+t213005@users.noreply.github.com>
2023-01-19 16:20:22 +0000

    1st

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import streamlit as st
+from janome.tokenizer import Tokenizer
 
-st.title("Word to Sentence Generator")
+def generate_sentence(word):
+    t = Tokenizer()
+    tokens = t.tokenize(word)
+    sentence = "入力した単語は"
+    for token in tokens:
+        sentence += token.surface
+    sentence += "です。"
+    return sentence
 
-word = st.text_input("Enter a word:")
+st.title("単語から文章生成アプリ(Japanese)")
 
-if st.button("Generate"):
-    # Insert code here to generate a sentence using the input word
-    sentence = "The " + word + " jumped over the moon."
+word = st.text_input("単語を入力:")
+
+if st.button("生成"):
+    sentence = generate_sentence(word)
     st.success(sentence)
