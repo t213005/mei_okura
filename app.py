01ea132203e8128154cf34d1490519544c977409
t213005 <121270907+t213005@users.noreply.github.com>
2023-01-19 18:21:31 +0000

    1st

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import streamlit as st
-from janome.tokenizer import Tokenizer
+import random
 
-def generate_sentence(word):
-    t = Tokenizer()
-    tokens = t.tokenize(word)
-    sentence = "入力した単語は"
-    for token in tokens:
-        sentence += token.surface
-    sentence += "です。"
-    return sentence
+# おみくじの結果を格納したリスト
+omikuji_list = ["大吉", "中吉", "小吉", "吉", "末吉", "凶"]
 
-st.title("単語から文章生成アプリ(Japanese)")
+def main():
+    st.title("おみくじアプリ")
 
-word = st.text_input("単語を入力:")
+    # おみくじを引くボタン
+    if st.button("おみくじを引く"):
+        result = random.choice(omikuji_list)
+        st.success(result)
 
-if st.button("生成"):
-    sentence = generate_sentence(word)
-    st.success(sentence)
+if __name__ == "__main__":
+    main()
