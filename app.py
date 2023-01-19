729d66ca0babb0dde4ba35a3677c714dcd68403b
t213005 <121270907+t213005@users.noreply.github.com>
2023-01-19 16:06:54 +0000

    1st

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import streamlit as st
-from transformers import AutoModelWithLMHead, AutoTokenizer
 
-# Load the pre-trained model and tokenizer
-model = AutoModelWithLMHead.from_pretrained("text-davinci-002")
-tokenizer = AutoTokenizer.from_pretrained("text-davinci-002")
+st.title("Word to Sentence Generator")
 
-st.title("Text Generation App")
+word = st.text_input("Enter a word:")
 
-# Get the user's input
-prompt = st.text_input("Enter your prompt:")
-
-# Generate text
-if st.button("Generate text"):
-    input_ids = tokenizer.encode(prompt, return_tensors="pt").to('cuda')
-    output = model.generate(input_ids, max_length=100, do_sample=True, top_p=0.95, top_k=50)
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    st.write("Generated text:")
-    st.write(generated_text)
+if st.button("Generate"):
+    # Insert code here to generate a sentence using the input word
+    sentence = "The " + word + " jumped over the moon."
+    st.success(sentence)
